7d3369fe29450351c24301e3680b307f1fcbdd63
Jeff Millar <jeff.millar@sbcglobal.net>
2013-12-27 15:40:06 -0800

    save data to a spreadsheet

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,8 +6,8 @@ class ContactsController < ApplicationController
 
   def create
     @contact = Contact.new(secure_params)
-    if @contact.valid?
-      # TODO save data
+    if @contact.valid?    
+      @contact.update_spreadsheet
       # TODO send message
       flash[:notice] = "Message sent from #{@contact.name}."
       redirect_to root_path
